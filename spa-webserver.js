84a8792a4ee53f1d34a59261c55306bff4cc4c52
Baz <baz@thinkloop.com>
2016-10-17 22:52:33 +0000

    match closest index folder rather than root

--- spa-webserver.js
+++ spa-webserver.js
@@ -5,13 +5,13 @@ var nodeStatic = require("node-static"),
 	http = require("http"),
 	chalk = require("chalk"),
 	getopt = require("posix-getopt"),
-	execFile = require('child_process').execFile,
-	parser = new getopt.BasicParser("d:(directory)s(ssl)p:(port)", process.argv),
-	opt,
+	execSync = require('child_process').execSync,
+	parser = new getopt.BasicParser("d:(directory)s(ssl)p:(port)", process.argv);
+
+var opt,
 	directory,
-	port,
-	files,
-	indexes = {};
+	port;
+
 
 // parse passed-in cli options: -d (directory), -p (port)
 while ((opt = parser.getopt()) !== undefined) {
@@ -31,16 +31,17 @@ port = port || process.env.PORT || 8080;
 
 log('*********************************************************************');
 
-// create index of file paths to see if they exist later
-execFile('find', [directory, ' -type f '], function(doNotHandleThisErr, stdout, stderr) {
-	files = stdout.split('\n')
-		.filter(function(path) {
-			if (path.length && path !== directory) {
-				return true;
-			}
-			return false;
-		})
-		.reduce(function(p, path) {
+var indexes = {};
+var files =
+	execSync(`find ${directory} -type f`)
+	.toString()
+	.split('\n').filter(function(path) {
+		if (path.length && path !== directory) {
+			return true;
+		}
+		return false;
+	})
+	.reduce(function(p, path) {
 			var cleanPath = path.replace(directory, '/').replace(/[/]+/, '/'),
 				indexFolder = cleanPath.replace(/index.*html/, '');
 
@@ -55,39 +56,46 @@ execFile('find', [directory, ' -type f '], function(doNotHandleThisErr, stdout, 
 			return p;
 		}, {});
 
-	startServer();
-});
+log('*********************************************************************');
 
-function startServer() {
-	log('*********************************************************************');
+var staticServer = new(nodeStatic.Server)(directory, {cache: 600});
 
-	var staticServer = new(nodeStatic.Server)(directory, {cache: 600});
+http.createServer(serveHTTP).listen(port);
 
-	http.createServer(serveHTTP).listen(port);
+log(`http://${process.env.C9_HOSTNAME || 'localhost'}/`);
 
-	log(`http://${process.env.C9_HOSTNAME || 'localhost'}/`);
+function serveHTTP(req, res) {
 
-	function serveHTTP(req, res) {
+	// if the file doesn't exist, find the closest index file
+	if (!files[req.url]) {
+		var splitURL = req.url.split('/'),
+			joinURL,
+			joinURLWithSlash;
 
-		// if the file doesn't exist, use the default index file
-		if (!files[req.url]) {
-			req.url = indexes[req.url] || indexes['/'];
+		splitURL.push('dummy element to be popped on the first iteration');
+
+		do {
+			splitURL.pop();
+			joinURL = splitURL.join('/'),
+			joinURLWithSlash = `${joinURL}/`.replace(/[/]+/g, '/');
+			req.url = indexes[joinURL] || indexes[joinURLWithSlash];
 		}
-
-		res.setHeader('X-Frame-Options', 'DENY');
-		res.setHeader("X-XSS-Protection", "1; mode=block");
-
-		staticServer.serve(req, res, function (err, result) {
-			if (err) {
-				console.error("Error serving %s: %s", req.url, err.message);
-				res.writeHead(err.status, err.headers);
-				res.end();
-			}
-			else {
-				log(req.url);
-			}
-		});
+		while (!req.url && splitURL.length);
 	}
+
+	res.setHeader('X-Frame-Options', 'DENY');
+	res.setHeader("X-XSS-Protection", "1; mode=block");
+
+	staticServer.serve(req, res, function (err, result) {
+		if (err) {
+			console.error("Error serving %s: %s", req.url, err.message);
+			res.writeHead(err.status, err.headers);
+			res.end();
+		}
+		else {
+			log(req.url);
+		}
+	});
 }
 
 function log(str) {
